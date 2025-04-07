d6d2ba1c2bf830b68510ed51caf6b062c27155d3
FiedorPW <mikolajfiedorczuk@gmail.com>
2025-04-07 22:19:42 +0200

    Ulepszenie uczenia

--- YOLOv11-fine-tuning/download_dataset.py
+++ YOLOv11-fine-tuning/download_dataset.py
@@ -1,5 +1,5 @@
 from roboflow import Roboflow
-rf = Roboflow(api_key="API_KEY")
+rf = Roboflow(api_key="Zz5nOrbtGm8uqusXKTgY")
 project = rf.workspace("roboflow-100").project("construction-safety-gsnvb")
 version = project.version(2)
 dataset = version.download("yolov11")
--- YOLOv11-fine-tuning/training.py
+++ YOLOv11-fine-tuning/training.py
@@ -1,31 +1,56 @@
 from ultralytics import YOLO
 import torch
 from multiprocessing import freeze_support
+import os
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# path to datasets yaml file
+data_yaml_path = os.path.join(script_dir, 'construction-safety-2/data.yaml')
+model_size = "n" #n,s,m
+batch = 10 # how much vram do you have? size 150 for 48GB
+
+
+def set_device():
+    '''
+    Fun to set computing device depending on the amount(1 or 2) GPUs or no GPU - using cpu
+    '''
+    # print("GPU Available:", torch.cuda.is_available())
+    # check% if and how much gpus there are
+    num_gpus = torch.cuda.device_count()
+    if num_gpus == 0:
+        device = "cpu"
+        print("\033[91mno GPU switching to CPU\033[0m")
+    elif num_gpus == 1:
+        device = "0"
+        print(f"\033[94mone GPU with {torch.cuda.get_device_properties(0).total_memory / 1e9:.1f} GB vRAM\033[0m")
+    elif num_gpus == 2:
+        device = "0,1"
+        combined_vram = (torch.cuda.get_device_properties(0).total_memory + torch.cuda.get_device_properties(1).total_memory) / 1e9
+        print(f"\033[94mtwo GPUs with {combined_vram:.1f} GB vRAM\033[0m")
+    return device
 
 if __name__ == '__main__':
     freeze_support()
 
-    print("GPU Available: ", torch.cuda.is_available())
-    print("GPU Name: ", torch.cuda.get_device_name(0))
+    device = set_device()
+    model = YOLO(f"yolo11{model_size}.pt")
 
-    # Load a COCO-pretrained YOLO11n model
-    model = YOLO("yolo11n.pt")
-
-    # Train the model on the COCO8 example dataset for 100 epochs
+    # Train the model on the dataset for 200 epochs using both GPUs
     results = model.train(
-            data="/home/ai/YOLOv11n-fine-tuning/construction-safety-2/data.yaml",
-        epochs=200, 
+        data=data_yaml_path,
+        epochs=200,
         imgsz=640,
-        device=0,
+        batch=batch,
+        device=device,
         name="train"
-        )
-    
+    )
+
     # Evaluate model performance on the validation set
     metrics = model.val()
 
     # Perform object detection on an image
-    results = model("YOUR_TESTING_IMAGE_PATH_HERE")
+    results = model("/home/ai/YOLOv11n-fine-tuning/construction-safety-2/test/images/ppe_0004_jpg.rf.c3265071237f64a4f0c3e1bf3048f923.jpg")
     results[0].show()
 
     # Export the model to ONNX format
-    path = model.export(format="onnx")  # return path to exported model
+    path = model.export(format="pt")  # Returns path to the exported model
